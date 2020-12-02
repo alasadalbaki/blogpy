9faa3faac7601c29e70539092d0195420438e050
alasadalbaki <alasadalbaki@gmail.com>
2020-12-02 15:45:05 +0330

    init

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls ), #"http://127.0.0.1/admin"
+    url('ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
